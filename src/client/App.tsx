016274a5d3d478b42550c9d9f1b88f96198ce202
Kenji Nakano <kenji.nakano@inf.ethz.ch>
2024-10-29 18:06:46 +0100

    Everything renders, next implement column selection, sorting, and state keeping

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import Layout from "./Layout";
-import { json2csv } from "json-2-csv";
-
+import React, { cloneElement, useEffect, useState }  from "react";
+// import { csv2json } from "json-2-csv";
 
 // Main application component
 /**
@@ -9,14 +9,115 @@ import { json2csv } from "json-2-csv";
  *
  * @returns {JSX.Element} - Returns the JSX structure for the application layout.
  */
+interface SelectedFile {
+  colNames: string[];
+  data: Array<Record<string, unknown>>;
+}
+
+interface SelectedColumn {
+  colName: string | undefined;
+  isNumeric: boolean;
+  ascending: boolean;
+}
+
+const SelectedFileContext = React.createContext<SelectedFile>({ colNames: [], data: [] });
+const SelectedColumnContext = React.createContext<SelectedColumn>({ colName: undefined, isNumeric: false, ascending: true })
+
+
 function App(): JSX.Element {
+  const [storedFiles, setStoredFiles] = useState<Array<string>>([]);
+  const [selectedFile, setSelectedFile] = useState<SelectedFile>({ colNames: [], data: [] });
+  // const [selectedColumn, setSelectedColumn] = 
+
+  const loadStoredFiles = async () => {
+    // update the list of stored files
+    try {
+      const response = await fetch("/api/getFileNames", {
+        method: "GET"
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to get file names");
+      }
+
+      const currStoredFiles = await response.json() as Array<string>;
+
+      setStoredFiles(currStoredFiles);
+
+
+    } catch (error) {
+      console.error("Error getting file names", error);
+    }
+  }
+
+  const onFileUpload = async (event: React.ChangeEvent<HTMLInputElement>): Promise<void> => {
+    if (!event.target.files) return;
+
+    //initialize the form data
+    const formData = new FormData();
+    formData.append("file", event.target.files[0] as File);
+
+    try {
+      const response = await fetch("/api/upload", {
+        method: "POST",
+        body: formData
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to upload file");
+      }
+      
+      await loadStoredFiles();
+
+
+      // todo: further process
+    } catch (error) {
+      console.error("Error uploading file", error);
+    }
+  }
+  
+  // when one of the uploaded files is selected, update the state of the selected file
+  const onFileChange = async (fileName: string): Promise<void> => {
+    try {
+      const response = await fetch("/api/getFileData/"+fileName);
+      if (!response.ok) {
+        throw new Error("Failed to get file data");
+      }
+
+      const fileJson = await response.json();
+      
+
+      if (fileJson.length === 0) {
+        throw new Error("Empty file");
+      }
+
+      setSelectedFile({
+        colNames: Object.keys(fileJson[0] as Record<string, unknown>),
+        data: fileJson
+      });
+
+
+    } catch (error) {
+      console.error("Error getting file data", error);
+    }
+  }
+
+
+  // load the stored files on initial render
+  useEffect(() => {
+    onFileChange("1730219014839-KANTON_SMALL.csv");
+    loadStoredFiles();
+  }, []);
+
   return (
     <Layout>
-      <FileInputCard />
+      <SelectedFileContext.Provider value={selectedFile}>
+      <FileInputCard onFileUpload={onFileUpload} />
 
-      <FileInfoCard fileName="testName" fileType="testType" fileSize={-1} fileRows={-1} />
+      <FileInfoCard storedFiles={storedFiles}/>
 
-      <DataTableWrapper tableColumnHeadings={<></>} tableRows={<></>} />
+      <DataTable />
+      </SelectedFileContext.Provider>
     </Layout>
   );
 }
@@ -28,16 +129,16 @@ function App(): JSX.Element {
  *
  * @returns {JSX.Element} - JSX structure for the file input card UI, allowing users to upload CSV data.
  */
-function FileInputCard(): JSX.Element {
-
-
-
+function FileInputCard({ onFileUpload }: { onFileUpload: (event: React.ChangeEvent<HTMLInputElement>) => Promise<void> }): JSX.Element {
   return (
     <article>
     <header>
       <h2>Select csv data</h2>
     </header>
-    <form id="select-data-form" action="/csvfiles" method="post" encType="multipart/form-data">
+    <form 
+      id="select-data-form"
+      // encType="multipart/form-data"
+      >
     <label htmlFor="file-input" className="custom-file-upload">
         <i className="fa fa-file-csv"></i> Select csv file to explore
       </label>
@@ -46,7 +147,8 @@ function FileInputCard(): JSX.Element {
         type="file"
         aria-describedby="fileHelp"
         accept="text/csv"
-        name="uploaded_file"
+        // name="uploaded_file"
+        onChange={onFileUpload}
         />
         <small id="fileHelp"
           >Please upload a csv file, where the first row is the header. And
@@ -58,78 +160,159 @@ function FileInputCard(): JSX.Element {
 
 
 
-/**
- * FileInfoCard Component - Displays metadata information about an uploaded file, including its name, type, size, and row count.
- *
- * @param {Object} props - Properties passed to the component.
- * @param {string} props.fileName - The name of the uploaded file.
- * @param {string} props.fileType - The MIME type of the file.
- * @param {number} props.fileSize - The size of the file in bytes.
- * @param {number} props.fileRows - The number of data rows in the file.
- *
- * @returns {JSX.Element} - A structured card element that lists file information.
- */
-function FileInfoCard({
-  fileName,
-  fileType,
-  fileSize,
-  fileRows
-}: {
-  fileName: string;
-  fileType: string;
-  fileSize: number;
-  fileRows: number;
-}): JSX.Element {
+function FileInfoCard(props: { storedFiles: Array<string> }) : JSX.Element {
+  let count = 1;
+
   return (
     <article className="file-info-card">
       <header className="file-info-card__header">
-        <h2>File Information</h2>
+        <h2>Data infos</h2>
       </header>
-      <ul className="file-info-card__details">
-        {/* File name */}
-        <li className="file-info-card__item">
-          <div className="file-info-card__label">File Name:</div>
-          <div className="file-info-card__value">{fileName}</div>
-        </li>
-        {/* File type */}
-        <li className="file-info-card__item">
-          <div className="file-info-card__label">File Type:</div>
-          <div className="file-info-card__value">{fileType}</div>
-        </li>
-        {/* File size */}
-        <li className="file-info-card__item">
-          <div className="file-info-card__label">File Size:</div>
-          <div className="file-info-card__value">{fileSize.toLocaleString()} bytes</div>
-        </li>
-        {/* Number of rows */}
-        <li className="file-info-card__item">
-          <div className="file-info-card__label">Number of Rows:</div>
-          <div className="file-info-card__value">{fileRows.toLocaleString()}</div>
-        </li>
-      </ul>
+      <table>
+        <thead className="file-info-card__table__header">
+          <tr>
+            <th>#</th>
+            <th>File name</th>
+            <th>Uploaded</th>
+            <th style={{ textAlign: "center"}}>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.storedFiles.map((fileName : string) => {
+            return (
+              <tr key={fileName}>
+                <td style={{ padding: "var(--spacing)"}}>{count++}</td>
+                <td>{getFileName(fileName)}</td>
+                <td>{getTimeString(fileName)}</td>
+                <td style={{ textAlign: "center"}}>
+                 <i className="fa fa-eye" style={{ textAlign: "center", padding: "0 var(--spacing)"}}></i>
+                 <i className="fa fa-trash" style={{ textAlign: "center"}}></i>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </article>
   );
 }
 
 
 
-function DataTableWrapper(props: {tableColumnHeadings: JSX.Element; tableRows: JSX.Element} = { tableColumnHeadings: <></>, tableRows: <></> }): JSX.Element {
+
+
+
+function DataTable(): JSX.Element {
+  const selectedFile = React.useContext(SelectedFileContext);
+  const [selectedColumn, setSelectedColumn] = useState<SelectedColumn>({ colName: undefined, isNumeric: false, ascending: true });
+
+
   return (
-    <article style={{ width: "100%" }}>
-      <header>
-        <h2>Data table</h2>
-      </header>
-      <figure id="table-content">
-        <table >
-          <thead id="table-content__headings">{props.tableColumnHeadings}</thead>
-          <tbody id="table-content__data">{props.tableRows}</tbody>
-        </table>
-      </figure>
-    </article>
+    <SelectedColumnContext.Provider value={selectedColumn}>
+      <article style={{ width: "100%" }}>
+        <header>
+          <h2>Data table</h2>
+        </header>
+        <figure id="table-content">
+          <table >
+            <thead id="table-content__headings">
+              <DataTableHeaderRow colNames={selectedFile.colNames}/>
+            </thead>
+            <tbody id="table-content__data">
+              <DataTableContent data={selectedFile.data}/>
+            </tbody>
+          </table>
+        </figure>
+      </article>
+    </SelectedColumnContext.Provider>
   );
 }
 
 
+function DataTableHeaderRow(props: { colNames: string[]}): JSX.Element {
+
+  return (
+    <tr>
+      {props.colNames.map((colName) => {
+        return <HeaderRowCell colName={colName}/>
+      })}
+    </tr>
+  )
+}
+
+function HeaderRowCell(props: { colName: string }): JSX.Element {
+  return (
+    <th
+      className="sortable">{props.colName}</th>
+  )
+}
+
+function DataTableContent(props: { data: Array<Record<string, unknown>> }): JSX.Element {
+  return (
+    <>
+      {props.data.map((row: Record<string, unknown>) => {
+        return <DataTableRow key={JSON.stringify(row)} row={row}/>
+      })}
+    </>
+  );
+}
+
+function DataTableRow(props: { row: Record<string, unknown> }): JSX.Element {
+  return (
+    <tr>
+      {Object.values(props.row).map((value) => {
+        return <DataTableCell value={value}/>
+      })}
+    </tr>
+  )
+}
+
+function DataTableCell(props: { value: unknown }): JSX.Element {
+  return (
+    <td>{props.value as string}</td>
+  )
+}
+
+
+// takes internal file name and returns the timestamp as a string
+function getTimeString(fileName: string): string {
+  const timestamp = Number(fileName.split("-")[0] as string);
+  return new Date(timestamp).toLocaleString();
+}
+
+// takes internal file name and returns the file name
+function getFileName(fileName: string): string {
+  return fileName.split("-").slice(1).join();
+}
+
+function columnIsNumeric(data: Array<Record<string, unknown>>, colName: string): boolean {
+  return data[0]?.[colName] instanceof Number;
+}
+
+function getComparator(colName: string, isNumeric: boolean, ascending: boolean): (a: Record<string, unknown>, b: Record<string, unknown>) => number {
+  let cmp_type:  (a: Record<string, unknown>, b: Record<string, unknown>) => number;
+  if (isNumeric) {
+    cmp_type = function (a: Record<string, unknown>, b: Record<string, unknown>): number {
+      return Number(a[colName]) - Number(b[colName]);
+    }
+  } else {
+    cmp_type = function (a: Record<string, unknown>, b: Record<string, unknown>): number {
+      return String(a[colName]).localeCompare(String(b[colName]));
+    }
+  }
+
+  let cmp_ordering: (a: Record<string, unknown>, b: Record<string, unknown>) => number;
+
+  if (ascending) {
+    cmp_ordering = cmp_type;
+  } else {
+    cmp_ordering = function (a: Record<string, unknown>, b: Record<string, unknown>): number {
+      return cmp_type(b, a);
+    }
+  }
+
+  return cmp_ordering;
+}
 
 
 
--- src/server/main.ts
+++ src/server/main.ts
@@ -1,15 +1,81 @@
 import express from "express";
 import ViteExpress from "vite-express";
+import multer from "multer";
+import fs from "fs";
+import { csv2json } from "json-2-csv";
 
 // creates the expres app do not change
 const app = express();
 
-// add your routes here
+/* ------------- Added Code ------------- */
 
-// example route which returns a message
-app.get("/hello", async function (_req, res) {
-  res.status(200).json({ message: "Hello World!" });
+// define where to store the uploaded files
+const storage = multer.diskStorage({
+  destination: function (_req, _file, cb) {
+    cb(null, "src/server/uploads/");
+  },
+
+  filename: function(_req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  }
 });
+
+const upload = multer({storage: storage});
+
+
+
+// route to handle file upload
+app.post("/api/upload", upload.single("file"), function (req, res) {
+  if (!req.file) {
+    res.status(400).send("No file uploaded");
+    return; 
+  }
+  console.log(req.file);
+  res.send("File uploaded successfully");
+})
+
+
+// route to save the clicked column per file
+
+// route to get stored names of all files
+app.get("/api/getFileNames", function (_reg, res) {
+  fs.readdir("src/server/uploads", function (err, files) {
+    if (err) {
+      res.status(500).send("Error reading files");
+      return;
+    }
+    console.log(files);
+    res.json(files);
+  });
+
+})
+
+//route to get data from a file
+app.get("/api/getFileData/:fileName", function (req, res) {
+  console.log("test");
+  console.log("Getting file data for: ", req.params.fileName);
+  const fileName= req.params.fileName;
+  const filePath = "src/server/uploads/"+fileName;
+  fs.access(filePath, fs.constants.F_OK, function (err) {
+
+  if (err) {
+    res.status(404).send("File not found");
+    return;
+  }
+
+    // read the file, send its contents
+    fs.readFile(filePath, "utf8", async function (err, data) {
+      if (err) {
+        console.error("Error reading file", err);
+        res.status(500).send("Error reading file");
+        return;
+      }
+      res.json(await csv2json(data));
+    });
+  });
+})
+
+/* ------------- Added Code ------------- */
 
 // Do not change below this line
 ViteExpress.listen(app, 5173, () =>
